70353a2be07c1301d8621c53a245e01727b048c7
“Carter <132953355+dudewherescarter@users.noreply.github.com>
2023-08-25 17:05:15 -0500

    Created function in script.js to randomly generate ComputerChoice

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-function getComputerChoice(a, b, c) {
-    let a = ("rock")
-    let b = ("Paper")
-    let c = ("Scissors")
+let playerScore = 0
+let computerScore = 0
+let roundWinner = ''
+
+function getComputerChoice() {
+    let choice = ["ROCK", "PAPER", "SCISSORS"];
+    let random = Math.floor(Math.random() * choice.length);
+    console.log(choice[random]);
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        roundWinner = 'tie'
+    } if (
+       (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+       (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+       (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+    ) {
+        playerScore++
+        roundWinner = 'Player'
+    } if  (
+       (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') ||
+       (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') ||
+       (playerSelection === 'ROCK' && computerSelection === 'PAPER')
+    ) {
+        computerScore++
+        roundWinner = 'Computer'
+    }
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
